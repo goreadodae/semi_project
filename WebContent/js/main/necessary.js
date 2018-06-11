61911df977d35cac90d00a42d44f5bb751623d90
jisoo <jong920224@naver.com>
2018-06-11 23:27:07 +0900

    0611_지수_커밋

--- WebContent/js/main/necessary.js
+++ WebContent/js/main/necessary.js
@@ -1,5 +1,18 @@
+// load header footer
 $(document).ready(function() {
-	// 로드
 	$("#header").load("/views/header/main-Header.jsp");
 	$("#footer").load("/views/footer/main-Footer.jsp");
 });
+
+// delete message
+function delete_info(obj) {
+	// 삭제할 ID 정보 찾기
+	var target = obj.parentNode.getAttribute('div');
+	console.log(target);
+	// 삭제할 element 찾기
+	var field = document.getElementById(target);
+	// #field 에서 삭제할 element 제거하기
+	document.getElementById('top-message').remove();
+
+}
+

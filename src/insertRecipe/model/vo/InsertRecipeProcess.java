fd0f6bbc3a96ee8a61013b1e41d76bb009d066dc
YEJINKIM <YEJINKIM@DESKTOP-OR0C045>
2018-06-20 16:32:04 +0900

    0620_예진

---
+++ src/insertRecipe/model/vo/InsertRecipeProcess.java
@@ -0,0 +1,75 @@
+package insertRecipe.model.vo;
+
+public class InsertRecipeProcess {
+	
+	private int processNo;
+	private int processOrder;
+	private String processExplain;
+	private String processPic;
+	private String ingre;
+	private String tools;
+	private String fireLevel;
+	private String tip;
+	private int recipeNo;
+	public InsertRecipeProcess() {}
+	public int getProcessNo() {
+		return processNo;
+	}
+	public void setProcessNo(int processNo) {
+		this.processNo = processNo;
+	}
+	public int getProcessOrder() {
+		return processOrder;
+	}
+	public void setProcessOrder(int processOrder) {
+		this.processOrder = processOrder;
+	}
+	public String getProcessExplain() {
+		return processExplain;
+	}
+	public void setProcessExplain(String processExplain) {
+		this.processExplain = processExplain;
+	}
+	public String getProcessPic() {
+		return processPic;
+	}
+	public void setProcessPic(String processPic) {
+		this.processPic = processPic;
+	}
+	public String getIngre() {
+		return ingre;
+	}
+	public void setIngre(String ingre) {
+		this.ingre = ingre;
+	}
+	public String getTools() {
+		return tools;
+	}
+	public void setTools(String tools) {
+		this.tools = tools;
+	}
+	public String getFireLevel() {
+		return fireLevel;
+	}
+	public void setFireLevel(String fireLevel) {
+		this.fireLevel = fireLevel;
+	}
+	public String getTip() {
+		return tip;
+	}
+	public void setTip(String tip) {
+		this.tip = tip;
+	}
+	public int getRecipeNo() {
+		return recipeNo;
+	}
+	public void setRecipeNo(int recipeNo) {
+		this.recipeNo = recipeNo;
+	}
+	
+	@Override
+	public String toString () {
+		return "processNo="+processNo+" ,processOrder="+processOrder+" ,processExplain="+processExplain+" ,processPic"+processPic+
+				" ,ingre="+ingre+" ,tools"+tools+" ,fireLevel"+fireLevel+" ,tip"+tip+" ,recipeNo"+recipeNo;
+	}
+}
